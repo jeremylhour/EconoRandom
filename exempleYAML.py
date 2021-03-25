f08b6aa23628446524b958a8acdd66ddca16167c
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2021-03-25 19:16:41 +0100

    Exemple utilisation YAML

---
+++ exempleYAML.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exemple utilisation de YAML
+
+Created on Thu Mar 25 19:05:19 2021
+
+@author: jeremylhour
+"""
+import yaml
+import os
+
+
+def fonction_exemple(methode):
+    """
+    fonction_exemple:
+        fonction pour montrer l'utilisation de YAML
+    
+    @param method (str): nom de la methode
+    """
+    methodes_possibles = ['methode1', 'methode2']
+    if not methode in methodes_possibles:
+        print(f'Aucune methode {methode}')
+    
+    if methode == 'methode1':
+        print('Vous avez choisi la méthode 1.')
+    elif methode == 'methode2':
+        print('vous avez choisi la méthode 2.')
+
+
+if __name__ == '__main__':
+    config_file = os.path.join(os.getcwd(),'exempleConfigYAML.yml')
+    print(os.getcwd())
+    with open(config_file, 'r') as stream:
+        config = yaml.safe_load(stream)
+    
+    fonction_exemple(methode = config['methode'])
